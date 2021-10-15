ae675719d55103c28502f719ea363da7051fd91f
Vincent Mouillot <vincent.mouillot@edu.univ-fcomte.fr>
2021-10-15 19:42:32 +0200

    API pour une date précise

--- APIessai.R
+++ APIessai.R
@@ -1,5 +1,9 @@
 library(httr)
 library(rjson)
 
-api<-"https://github.com/florianzemma/CoronavirusAPI-France"
-brut<-GET(api)
+
+ap<-"https://coronavirusapi-france.now.sh/AllDataByDate?date=2020-04-19"
+donneebr<-GET(ap)
+rawToChar(donneebr$content)
+liste<-fromJSON(rawToChar(donneebr$content)) #On obtient la liste des infos par dep pour une date précise
+names(liste)
