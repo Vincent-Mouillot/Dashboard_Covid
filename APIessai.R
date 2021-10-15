baa80c3876dc9cf54fde35d3e8829826c1dc966e
Vincent Mouillot <mouillotvincent@johndeere.com>
2021-10-15 14:53:20 +0200

    Essai API

--- APIessai.R
+++ APIessai.R
@@ -3,3 +3,9 @@ library(rjson)
 
 api<-"https://github.com/florianzemma/CoronavirusAPI-France"
 brut<-GET(api)
+rawToChar(brut$content)
+
+ap<-"https://coronavirusapi-france.now.sh/AllDataByDate?date=2020-04-19"
+bt<-GET(ap)
+
+apdep<-"https://coronavirusapi-france.now.sh/AllDataByDepartement?Departement=Rhône"
