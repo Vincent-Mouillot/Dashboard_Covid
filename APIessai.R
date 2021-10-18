a05184b7ddbbb759727200ab0f12e2f3a203deb2
Verdier-Claire <claire.verdier0@orange.fr>
2021-10-18 18:26:21 +0200

    changement dep

--- APIessai.R
+++ APIessai.R
@@ -8,23 +8,17 @@ library(rjson)
 date<-"2020-04-19" # devra être modifier lors de l'ajout dans l'ui/server
 api_date<- paste("https://coronavirusapi-france.now.sh/AllDataByDate?date=", date)
 Brut_api_date<-GET(api_date)
+rawToChar(Brut_api_date$content)
 liste_date<-fromJSON(rawToChar(Brut_api_date$content), method = "C") #je sais j'étudie la question
 names(liste_date) #devrait obtenir "code, nom, date, hospitalisation, reanimation etc
 #et non allFranceDataByDate : probleme dans GET et dans le format de Rjson
 
-
-
-
-ap<-"https://coronavirusapi-france.now.sh/AllDataByDate?date=2020-04-19"
-donneebr<-GET(ap)
-rawToChar(donneebr$content)
-liste<-fromJSON(rawToChar(donneebr$content)) #On obtient la liste des infos par dep pour une date précise
-
-
 hos<-liste$allFranceDataByDate%>%sapply("[",4)%>%as.data.frame()%>%sum()
+#On obtient la liste des infos par dep pour une date précise
 
 dep<-"Rhône"
-apdep<-paste("https://coronavirusapi-france.now.sh/AllDataByDepartement?Departement=Rhône", dep)
-donneedep<-GET(apdep)
+api_dep<-paste("https://coronavirusapi-france.now.sh/AllDataByDepartement?Departement=", dep)
+donneedep<-GET(api_dep)
+rawToChar(donneedep$content)
 listedep<-fromJSON(rawToChar(donneedep$content))
 df_bydep<-listedep$allDataByDepartement %>% sapply("[", 4) %>% as.data.frame() %>% sum()
